399feff74c8f21639baed7f2e2d0de8b928280d7
Adam Serafini <adam_serafini@yahoo.co.uk>
2012-09-16 10:11:18 +0100

    testing map vs. vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include "TestSuite.h"
 #include "SuffixTree.h"
 
+#include <time.h>
+
+#include "FASTA_FileReader.h"
+#include "GeneralSuffixTree.h"
+
 using namespace std;
 
 int main() {
-	SuffixTree tree;
-	tree.construct("xabxa$");
-	tree.log_tree();
+	//SuffixTree tree;
+	//tree.construct("xabxa$");
+	//tree.log_tree();
 
-	EXECUTE_TEST_SUITE();
+	//EXECUTE_TEST_SUITE();
+
+	FASTA_FileReader file("SWINEPOX_SIMULATED_READS.txt");
+
+	time_t start, end;
+	time (&start);
+	GeneralSuffixTree gst(file.parse());
+	time (&end);
+	double dif = difftime(end, start);
+
+	cout << "Took: " <<  dif << endl;
+
+	cout << "Done!" << endl;
+	cin.get();
+	
 }
 
