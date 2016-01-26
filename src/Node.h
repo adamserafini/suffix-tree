a9beac77b72a204392689e8254d1d08966349cf5
adam <adam_serafini@yahoo.co.uk>
2016-01-26 14:22:45 +0100

    Tidy formatting.

--- src/Node.h
+++ src/Node.h
@@ -13,18 +13,17 @@ public:
 	void add_child(const SuffixTree&, Node*);
 	void remove_child(const SuffixTree&, Node*);
 	bool is_leaf() {return children.empty();}
-    void split_edge(const SuffixTree&, int, int);
+  void split_edge(const SuffixTree&, int, int);
 	Node* get_child(const SuffixTree&, int char_index);
 	Node* get_char_child(const SuffixTree&, char ch);
 	void get_children(std::vector<Node*>&) const;
 	int get_key(const SuffixTree&, Node*, int) const;
 
-    Node* parent;
-    std::map<int, Node*> children;
+  Node* parent;
+  std::map<int, Node*> children;
 	std::vector<int> labels;
-    Node* suffix_link;
+  Node* suffix_link;
 	int begin_index;
 	int* end_index;
-    int ID;
+  int ID;
 };
-
